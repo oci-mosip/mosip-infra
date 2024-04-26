635059fcbd37d8e3d36eafa30e82a66c3d783b51
Subhash Chandra Budde <subhashchandra.b@gmail.com>
2024-04-26 17:20:08 +0530

    Removed external input prompts
    Signed-off-by: Subhash Chandra Budde <subhashchandra.b@gmail.com>

--- deployment/v3/mosip/masterdata-loader/install.sh
+++ deployment/v3/mosip/masterdata-loader/install.sh
@@ -7,28 +7,21 @@ if [ $# -ge 1 ] ; then
 fi
 
 echo  WARNING: This need to be executed only once at the begining for masterdata deployment. If reexecuted in a working env this will reset the whole master_data DB tables resulting in data loss.
-echo  Please skip this if masterdata is already uploaded.
-yn = "Y"
-#read -p "CAUTION: Do you still want to continue(Y/n)" yn
-if [ $yn = "Y" ] 
-  then
-   NS=masterdata-loader
-   CHART_VERSION=12.0.1-B2
-   helm delete masterdata-loader -n $NS
-   echo Create $NS namespace
-   kubectl create ns $NS
+NS=masterdata-loader
+CHART_VERSION=12.0.1-B2
+helm delete masterdata-loader -n $NS
+echo Create $NS namespace
+kubectl create ns $NS
 
-   echo Istio label
-   kubectl label ns $NS istio-injection=enabled --overwrite
-   helm repo update
+echo Istio label
+kubectl label ns $NS istio-injection=enabled --overwrite
+helm repo update
 
-   echo Copy configmaps
-   ./copy_secrets.sh
+echo Copy configmaps
+./copy_secrets.sh
 
-   echo Loading masterdata
-   helm -n $NS install masterdata-loader  mosip/masterdata-loader --set mosipDataGithubBranch=1.2.0.1 --version $CHART_VERSION --wait
+echo Loading masterdata
+helm -n $NS install masterdata-loader  mosip/masterdata-loader --set mosipDataGithubBranch=1.2.0.1 --version $CHART_VERSION --wait
 
-   else
-   break
 
-fi
+
