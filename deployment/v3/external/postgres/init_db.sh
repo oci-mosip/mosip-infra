ac39634f964b8dec25e245ce61083991d364e149
Subhash Chandra Budde <subhashchandra.b@gmail.com>
2024-05-05 14:40:14 +0530

    removed input prompts for automation
    Signed-off-by: Subhash Chandra Budde <subhashchandra.b@gmail.com>

--- deployment/v3/external/postgres/init_db.sh
+++ deployment/v3/external/postgres/init_db.sh
@@ -10,19 +10,10 @@ function initialize_db() {
   NS=postgres
   CHART_VERSION=12.0.1-B4
   helm repo update
-  while true; do
-      read -p "CAUTION: all existing data will be lost. Are you sure?(Y/n)" yn
-      if [ $yn = "Y" ]
-        then
-          echo Removing any existing installation
-          helm -n $NS delete postgres-init || true
-          echo Initializing DB
-          helm -n $NS install postgres-init mosip/postgres-init -f init_values.yaml --version $CHART_VERSION --wait --wait-for-jobs
-          break
-        else
-          break
-      fi
-  done
+  echo Removing any existing installation
+  helm -n $NS delete postgres-init || true
+  echo Initializing DB
+  helm -n $NS install postgres-init mosip/postgres-init -f init_values.yaml --version $CHART_VERSION --wait --wait-for-jobs
   return 0
 }
 
